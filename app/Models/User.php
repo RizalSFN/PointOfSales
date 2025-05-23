34ca3465bb1b79bcbba5c40ea0e70329879bce98
Rizal <rizalsofiana1976@gmail.com>
2025-05-23 21:45:00 +0700

    mengubah file migrasi & model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'nama',
         'email',
         'password',
+        'role',
+        'status'
     ];
 
     /**
@@ -41,7 +43,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
